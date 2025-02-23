0d0a6b773aca9557c043dacba961b8d28ffb5ca3
Leorev01 <revrennaleo@gmail.com>
2025-02-23 21:48:06 +0000

    Adds redirection from /create-event to /log-in if no user in localStorage

---
+++ app/create-event/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+//import { useSession } from "next-auth/react";
+
+export default function CreateEventLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  /*const { data: session, status } = useSession();
+
+  // Redirect if user is not authenticated
+  if (status === "loading") return <p>Loading...</p>;
+  if (!session) {
+    router.push("/log-in");
+    return null; // Prevents rendering content before redirection
+  }*/
+ if(!localStorage.getItem("user")){
+    router.push("/log-in");
+    return null;
+  }
+  else{
+    return <>{children}</>;
+  }
+}
