ece91f0356e1518849f0b19f55a7fd3e68ad11e8
Leorev01 <revrennaleo@gmail.com>
2025-03-06 20:04:54 +0000

    Completes login functionality

--- app/(forms)/log-in/page.tsx
+++ app/(forms)/log-in/page.tsx
@@ -3,7 +3,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
