cb349637e4329244726a3c4fe9b8be0f4d5c223c
Leorev01 <revrennaleo@gmail.com>
2025-04-20 19:48:49 +0100

    Adds organiser information to event id page

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -13,6 +13,7 @@ const EventDetailPage = () => {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [event, setEvent] = useState<any>(null);
+  const [organiser, setOrganiser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [showModal, setShowModal] = useState(false);
 
@@ -34,6 +35,33 @@ const EventDetailPage = () => {
     fetchEvent();
   }, [id]);
 
+  useEffect(() => {
+    const fetchOrganiser = async () => {
+      if (!event?.organiser_id) {
+        console.warn("Organiser ID is missing or invalid.");
+        return;
+      }
+  
+      const { data, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("id", event.organiser_id)
+        .single();
+  
+      if (error) {
+        console.error("Error fetching organiser:", error.message);
+      } else if (!data) {
+        console.warn("No organiser found for ID:", event.organiser_id);
+      } else {
+        setOrganiser(data);
+        console.log("Organiser fetched:", data);
+      }
+    };
+  
+    fetchOrganiser();
+  }, [event?.organiser_id]); // ✅ watch specific value, not whole event
+  
+
   if (loading) return <p>Loading event...</p>;
   if (!event) return <p>Event not found.</p>;
 
@@ -42,6 +70,30 @@ const EventDetailPage = () => {
       {showModal && <EventRegistrationModal eventId={id as string} onClose={() => setShowModal(false)} />}
       
       {/* Display event details */}
+      <div className="width-screen flex flex-row justify-around items-center border-b-2 border-[#2B2D42] mb-4 p-4">
+        <div className="flex flex-col items-start justify-center gap-5">
+          <h1 className="text-4xl font-bold text-[#2B2D42]">{event.name}</h1>
+          <p>
+            {organiser ? (
+                <Image
+                  src={organiser.avatar || "/images/default-avatar.png"}
+                  alt={`${organiser.name}'s profile pic`}
+                  width={40}
+                  height={30}
+                  className="inline-block rounded-full mr-5"
+                />
+            ) : (
+              "Loading organiser..."
+            )}
+            Hosted by:{organiser ? organiser.name : "Loading..."}
+          </p>
+          <p className="text-gray-600">Date: {new Date(event.date).toDateString()} : {event.time}</p>
+        </div>
+        <button className="bg-[#2B2D42] hover:bg-[#8D99AE] text-white text-lg font-semibold px-8 py-4 rounded-full transition-all duration-300 w-fit h-fit" 
+        onClick={() => setShowModal(true)}>
+          Register
+        </button>
+      </div>
       <div className="p-6 max-w-lg mx-auto bg-white shadow-md rounded-md">
         <h1 className="text-2xl font-bold mb-4">{event.name}</h1>
         <Image
