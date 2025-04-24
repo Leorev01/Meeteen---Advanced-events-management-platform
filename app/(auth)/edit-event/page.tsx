dd7e1e073671f26db40008c93297d3ff26a20488
Leorev01 <revrennaleo@gmail.com>
2025-04-24 18:34:25 +0100

    Wraps edit-event page with suspense to allow for usesearchparams to deploy on vercel

--- app/(auth)/edit-event/page.tsx
+++ app/(auth)/edit-event/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import {useSearchParams} from "next/navigation"
+import { useSearchParams } from "next/navigation"
 import { useRouter } from "next/navigation"
 import { supabase } from "@/lib/supabase"
 import { useEffect, useState } from "react"
