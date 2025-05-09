e75b35cc1fc3c4513615a374050f7a802e2b118b
Leorev01 <revrennaleo@gmail.com>
2025-05-09 21:06:09 +0100

    Adds categories section and uses mediaquery to improve responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import CategoriesSection from '@/components/HomePage/CategoriesSection'
 import EventsSection from '@/components/HomePage/EventsSection'
 import HeroSection from '@/components/HomePage/HeroSection'
 import JoinMeeteenSection from '@/components/HomePage/JoinMeeteenSection'
@@ -6,9 +7,8 @@ import UpcomingEventsSection from '@/components/HomePage/UpcomingEventsSection'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const Home = () => {
 
-  
+const Home = () => {
 
   return (
     <div className='max-w-7xl mx-auto'>
@@ -25,39 +25,7 @@ const Home = () => {
       <JoinMeeteenSection />
 
       {/* Explore Top Categories Section */}
-      <div>
-        <h3 className='text-3xl font-bold mt-20 mb-5'>Explore Top Categories</h3>
-        <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 mx-5 sm:mx-10 lg:mx-20'>
-          {[{
-            icon: '/images/categories/outdoors.png',
-            title: 'Outdoor',
-          },
-          {
-            icon: '/images/categories/tech.png',
-            title: 'Tech',
-          },
-          {
-            icon: '/images/categories/education.png',
-            title: 'Education',
-          },
-          {
-            icon: '/images/categories/music.png',
-            title: 'Music',
-          },
-          {
-            icon: '/images/categories/food.png',
-            title: 'Food',
-          },].map((category, index) => (
-            <Link href={`/search?query=${category.title.toLowerCase()}`} key={index} className='flex flex-col justify-evenly text-center text-xl w-40 bg-[#EDF2F4] p-4 rounded-lg shadow-2xl cursor-pointer hover:scale-105 transform transition duration-300'>
-              <Image  src={category.icon} alt={category.title} width={300} height={300} className='w-16 h-16 mx-auto mb-2'/>
-              <div>
-                <h4>{category.title}</h4>
-              </div>
-            </Link>
-          ))}
-        </div>
-        
-      </div>
+      <CategoriesSection />
 
       {/* Popular Events */}
       <PopularEvents />
---
+++ components/HomePage/CategoriesSection.tsx
@@ -0,0 +1,48 @@
+'use client'
+import useMediaQuery from '@/hooks/useMediaQuery'
+import Link from 'next/link';
+import Image from 'next/image';
+
+const categories = [{
+            icon: '/images/categories/outdoors.png',
+            title: 'Outdoor',
+          },
+          {
+            icon: '/images/categories/tech.png',
+            title: 'Tech',
+          },
+          {
+            icon: '/images/categories/education.png',
+            title: 'Education',
+          },
+          {
+            icon: '/images/categories/music.png',
+            title: 'Music',
+          },
+          {
+            icon: '/images/categories/food.png',
+            title: 'Food',
+          },]
+const CategoriesSection = () => {
+
+    const isMediumScreen = useMediaQuery('(min-width: 868px)');
+
+  return (
+    <div>
+        <h3 className='text-3xl font-bold mt-20 mb-5'>Explore Top Categories</h3>
+        <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 mx-5 sm:mx-10 lg:mx-20'>
+          {categories.slice(0, isMediumScreen ? 5 : 4).map((category, index) => (
+            <Link href={`/search?query=${category.title.toLowerCase()}`} key={index} className='flex flex-col justify-evenly text-center text-xl w-40 bg-[#EDF2F4] p-4 rounded-lg shadow-2xl cursor-pointer hover:scale-105 transform transition duration-300'>
+              <Image  src={category.icon} alt={category.title} width={300} height={300} className='w-16 h-16 mx-auto mb-2'/>
+              <div>
+                <h4>{category.title}</h4>
+              </div>
+            </Link>
+          ))}
+        </div>
+        
+      </div>
+  )
+}
+
+export default CategoriesSection
